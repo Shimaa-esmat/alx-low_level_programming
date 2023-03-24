6da48429a3f2bf5f45976f02535668aca9b082b9
Shimaa-esmat <shymaaesam01@gmail.com>
2023-03-23 18:10:34 -0700

    c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiblies two numbers
+ * @a: frist number
+ * @b:second number
+ * Return: return result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
