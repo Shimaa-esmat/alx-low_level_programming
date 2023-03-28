4f598ac0d6f91607f4f0afcbc052f7f9d6c4c205
Shimaa-esmat <shymaaesam01@gmail.com>
2023-03-27 17:37:03 -0700

    c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	for (cunter = 0; *s != '\0'; ++s)
+		++counter;
+
+	return (counter);
+}
