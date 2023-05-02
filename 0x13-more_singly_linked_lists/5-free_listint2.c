a790c5a761e2e4e6e90a4ad3052472977d748b90
shimaa esmat <shymaaesam01@gmail.com>
2023-05-03 02:07:24 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a list of int
- * @head:pointer the listint_t list to frist node
+ * free_listint2 - this function frees list of ints
+ * @head: address ofpointer to first node
  *
- * Return: void
+ * Description: this function frees a lists of ints
+ *
+ * Return: Description of the returned value
  */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *node, *temp;
@@ -14,7 +17,7 @@ void free_listint2(listint_t **head)
 		return;
 
 	node = *head;
-	while(node)
+	while (node)
 	{
 		temp = node;
 		node = node->next;
